be5b8bae4074115e89b1c442e1de72a8abc13811
Bruno Parga <brunoparga@gmail.com>
2020-09-24 21:05:55 +0300

    Begin implementing the function with TCR
    TCR stands for "test && commit || revert"

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,13 @@
-import { message } from '.'
+import { combineReducers } from '.'
 
-test('Greets the world. How cliché!', () => {
-  expect(message()).toBe('Hello world!')
+test('Works with an empty state and identity action', () => {
+  const appState = {}
+  const action = { type: 'identity' }
+  expect(combineReducers({})(appState, action)).toStrictEqual({})
+})
+
+test('Works with a non-empty state and identity action', () => {
+  const appState = { foo: 'bar' }
+  const action = { type: 'identity' }
+  expect(combineReducers({})(appState, action)).toStrictEqual({})
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export const message = () => 'Hello world!'
+type Action = { type: string, payload?: any }
+type Reducer<T> = (appState: T, action: Action) => T
+type SliceReducer<T, K extends keyof T> = (
+  (appState: T, action: Action) => Record<K, T[K]>
+)
+
+export function combineReducers<T> (x: T): Reducer<T> { return () => x }
