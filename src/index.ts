d184ed3c9cd832e15019423763e12fcbd34bcf31
Bruno Parga <brunoparga@gmail.com>
2020-09-25 16:20:51 +0300

    Implement function for simple states

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-type Action = { type: string, payload?: any }
-type Reducer<T> = (appState: T, action: Action) => T
-type SliceReducer<T, K extends keyof T> = (
+export type Action = { type: string, payload?: any }
+export type Reducer<T> = (appState: T, action: Action) => T
+export type SliceReducer<T, K extends keyof T> = (
   (appState: T, action: Action) => Record<K, T[K]>
 )
+type argumentSlice<T, K extends keyof T> = Record<K, T[K]>
 
-export function combineReducers<T> (x: T): Reducer<T> { return () => x }
+export default function combineReducers<T>
+(sliceReducers: argumentSlice<T, keyof T>): Reducer<T> {
+  return (appState: T, action: Action) => {
+    const entries = Object.entries(sliceReducers) as [string, Function][]
+    return entries.reduce((state, [slice, sliceReducer]) => {
+      return { ...state, [slice]: sliceReducer(appState, action)[slice] }
+    }, appState) as unknown as T
+  }
+}
