43ea775902ee809498942ebd2d14d6bdbe8dbc27
Bruno Parga <brunoparga@gmail.com>
2020-09-26 21:59:06 +0300

    I am thoroughly confused.

--- src/index.test.ts
+++ src/index.test.ts
@@ -23,16 +23,16 @@ describe('Simple states', () => {
 
 type AppState = { foo: string, quux: string }
 const fooReducer: SliceReducer<AppState, 'foo'> =
-(state: AppState, { type }: Action): Record<'foo', AppState['foo']> => {
-  switch (type) {
-    case 'twodown':
-      return { foo: 'baz' }
-    case 'garplify':
-      return { foo: 'garply' }
-    default:
-      return { foo: state.foo }
+  (state: AppState, { type }: Action) => {
+    switch (type) {
+      case 'twodown':
+        return { foo: 'baz' }
+      case 'garplify':
+        return { foo: 'garply' }
+      default:
+        return { foo: state.foo }
+    }
   }
-}
 
 describe('States with two attributes', () => {
   const state: AppState = { foo: 'bar', quux: 'corge' }
@@ -87,6 +87,20 @@ describe('States with nested objects', () => {
     expect(newState).toStrictEqual(expectedState)
   })
 
-  // it('Works with only a nested-level property changing')
+  it('Works with only a nested-level property changing', () => {
+    const corgeReducer: any = (state: DeepAppState, { type }: Action) => {
+      if (type === 'toggle') {
+        console.log("Yeah I'm definitely here")
+        return { ...state, bar: { ...state.bar, corge: !state.bar.corge } }
+      }
+      return { corge: state.bar.corge }
+    }
+    const reducer = combineReducers(
+      { foo: idFooReducer, bar: { quux: idQuuxReducer, corge: corgeReducer } }
+    )
+    const newState = reducer(state, { type: 'toggle' })
+    const expectedState: DeepAppState = { foo: 'baz', bar: { quux: 42, corge: true } }
+    expect(newState).toStrictEqual(expectedState)
+  })
   // it('Works with properties on both levels changing')
 })
